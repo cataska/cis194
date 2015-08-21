3905dfffa595f8d34b37ef4b2dd4cb9f02a379c9
Wen-Chun Lin <lovecankill@gmail.com>
2015-08-21 22:55:01 +0800

    Finish exercise 4

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -25,3 +25,33 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+instance Expr Integer where
+  lit x = x
+  add expr1 expr2 = expr1 + expr2
+  mul expr1 expr2 = expr1 * expr2
+
+instance Expr Bool where
+  lit x = x <= 0
+  add expr1 expr2 = expr1 || expr2
+  mul expr1 expr2 = expr1 && expr2
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax a) (MinMax b) = MinMax (max a b)
+  mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 a) (Mod7 b) = Mod7 ((a+b) `mod` 7)
+  mul (Mod7 a) (Mod7 b) = Mod7 ((a*b) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger  = testExp :: Maybe Integer
+testBool     = testExp :: Maybe Bool
+testMM       = testExp :: Maybe MinMax
+testSat      = testExp :: Maybe Mod7
